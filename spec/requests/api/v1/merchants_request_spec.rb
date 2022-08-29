87df32d6edf15c63a695c4e882b0bbd1a1a53acc
John Santosuosso <john.santosuosso1@gmail.com>
2022-08-29 15:11:33 -0400

    partial test passing to find merchant by id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -19,7 +19,13 @@ describe "Merchants API" do
       expect(merchant).to have_key(:name)
       expect(merchant[:name]).to be_a(String)
     end
+  end
 
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
 
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
   end
 end
