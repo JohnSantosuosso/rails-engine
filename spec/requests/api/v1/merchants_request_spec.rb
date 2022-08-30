902eb668777a428081cb890d6de92c0eb87c2d6a
John Santosuosso <john.santosuosso1@gmail.com>
2022-08-30 18:32:38 -0400

    fix get all merchant test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,23 +1,26 @@
 require 'rails_helper'
 
 describe "Merchants API" do
+  #Happy Path Tests
   it "gets all merchants" do
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
 
-    expect(response).to be_successful
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    
+    merchants = response_body[:data]
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
 
     expect(merchants.count).to eq(3)
 
     merchants.each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:id]).to be_a(String)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      expect(merchant).to have_key(:attributes)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
 
