87991fc07f3e94acaaa669493f80979a89651f3a
John Santosuosso <john.santosuosso1@gmail.com>
2022-08-29 22:43:36 -0400

    add passing test to get merchants items

--- spec/requests/api/v1/merchant_items_spec.rb
+++ spec/requests/api/v1/merchant_items_spec.rb
@@ -11,6 +11,23 @@ describe "Merchant Items API" do
 
     expect(response).to be_successful
 
+    merchant_items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant_items.count).to eq(3)
+
+    merchant_items.each do |item|
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_an(Integer)
+
+    expect(item).to have_key(:name)
+    expect(item[:name]).to be_a(String)
+
+    expect(item).to have_key(:description)
+    expect(item[:description]).to be_a(String)
+
+    expect(item).to have_key(:unit_price)
+    expect(item[:unit_price]).to be_a(Float)
+    end
   end
 
 end
