bab654319e531cfa8cd9d06b1548b1bb5dbc8cc3
John Santosuosso <john.santosuosso1@gmail.com>
2022-08-31 14:34:49 -0400

    add test finds items with partial match

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -3,10 +3,10 @@ class Api::V1::SearchesController < ApplicationController
   def find_all
     if params[:name]
       items = Item.where("name ILIKE ?", "%#{params[:name]}%")
-      if items = []
-        render json: { data: { message: 'No Item Found' } }
-      else
+      if !items.nil?
         render json: ItemSerializer.new(items)
+      else
+        render json: { data: { message: 'No Match Found' } } 
       end
     else
       render status: 404
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -142,7 +142,24 @@ describe "Items API" do
 
     get "/api/v1/items/find_all?name=Air" #passes params name: "Air"
 
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    
+    items = response_body[:data]
+
     expect(response).to be_successful
+
+    expect(items.count).to eq(2)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(String)
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes][:name]).to be_a(String)
+      expect(item[:attributes][:description]).to be_a(String)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+    end
+
   end
 
   it "finds all items by name, sad path" do
