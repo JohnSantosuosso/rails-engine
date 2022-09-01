780b9a5d9538ce391ff7acbd4fad9681094fb0c9
John Santosuosso <john.santosuosso1@gmail.com>
2022-09-01 16:37:26 -0400

    add associations to items, invoices, invoice_items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+  has_many :invoice_items
+  has_many :invoices through :invoice_items
 
   validates_presence_of :name, type: :string
   validates_presence_of :description, type: :string
